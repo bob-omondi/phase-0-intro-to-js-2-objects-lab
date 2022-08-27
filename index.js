891e1db630a91829e745d48b2cd30d1ed6839aed
+ <+>
2022-08-27 15:03:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-employee = ["name", "streetAddress"]
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    employee[key] = value;
-    return employee;
+const employee = {};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newObj = { ...employee, streetAddress: "11 Broadway" };
+  return newObj;
 }
-function updateEmployeeWithKeyAndValue2(employee, key, value) {
-    const newEmployee = { ...employee };
-    newEmployee[key] = value;
-    return newEmployee;
-  }
-  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
-  }
-  function deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = { ...employee };
-    newEmployee[value];
-    delete newEmployee[value];
-    return newEmployee;
-  }
-  function destructivelyDeleteEmployeeByKey(object, key) {
-    delete object[key];
-    return object;
-  }
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmp = { ...employee[key] };
+  delete newEmp[key];
+  return newEmp;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+module.exports = {
+  updateEmployeeWithKeyAndValue,
+  deleteFromEmployeeByKey,
+  destructivelyUpdateEmployeeWithKeyAndValue,
+  destructivelyDeleteFromEmployeeByKey,
+};
